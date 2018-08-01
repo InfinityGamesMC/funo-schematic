7cd3e4d39565c5a150bf6c93626b9adca80011a0
Luke <luke.wyatt21@gmail.com>
2018-08-01 21:38:09 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,30 @@
+# funo-schematic
+A simple schematic library for GoMint.
+
+### Loading a schematic
+```java
+FunoSchematic schematic = new FunoSchematic.Loader(new File("test.schematic")).load();
+
+// To access schematic data
+FunoSchematic.SchematicData data = schematic.getSchematicData();
+```
+
+### Creating a schematic
+```java
+// First method
+FunoSchematic schematic = new FunoSchematic(new FunoSchematic.SchematicData(length, width, height));
+FunoSchematic schematic = new FunoSchematic(new FunoSchematic.SchematicData(length, width, height, blocks));
+FunoSchematic schematic = new FunoSchematic(new FunoSchematic.SchematicData(length, width, height, blocks, blockData));
+
+// Second method
+FunoSchematic.SchematicData data = new FunoSchematic.SchematicData();
+data.length = 100;
+data.width = 100;
+data.height = 100;
+data.blocks = new byte[]{1, 2, 3};
+data.blockData = new byte[]{1, 2, 3};
+
+FunoSchematic schematic = new FunoSchematic(data);
+```
+
+NOTE: Block ids and block data must be be at the same index in their respective byte arrays!
---
+++ src/main/java/host/funo/schematic/FunoSchematic.java
@@ -0,0 +1,129 @@
+package host.funo.schematic;
+
+import io.gomint.GoMint;
+import io.gomint.server.GoMintServer;
+import io.gomint.server.world.WorldAdapter;
+import io.gomint.world.World;
+import io.gomint.math.BlockPosition;
+import io.gomint.taglib.NBTStream;
+
+import java.io.*;
+import java.nio.ByteOrder;
+import java.util.zip.GZIPInputStream;
+
+public class FunoSchematic {
+    private SchematicData schematicData;
+
+    public FunoSchematic( SchematicData data ) {
+        this.schematicData = data;
+    }
+
+    /**
+     * Paste the current schematic into the world at the given position.
+     *
+     * @param world the world in which to paste the schematic
+     * @param position the position at which to paste the schematic
+     */
+    public void paste( World world, BlockPosition position ) {
+        GoMintServer server = (GoMintServer) GoMint.instance();
+
+        for ( int x = 0; x < this.schematicData.width; x++ ) {
+            for ( int y = 0; y < this.schematicData.height; y++ ) {
+                for ( int z = 0; z < this.schematicData.length; z++ ) {
+                    int index = y * this.schematicData.width * this.schematicData.length + z * this.schematicData.width + x;
+
+                    WorldAdapter worldAdapter = (WorldAdapter) server.getWorld( world.getWorldName() );
+                    worldAdapter.setBlockId( position, -1, this.schematicData.blocks[index] );
+                    worldAdapter.setBlockData( position, -1, this.schematicData.blockData[index] );
+                }
+            }
+        }
+    }
+
+    /**
+     * @return the schematic data
+     */
+    public SchematicData getSchematicData() {
+        return this.schematicData;
+    }
+
+
+    public static class SchematicLoader {
+        private File schematicFile;
+
+        /**
+         * @param schematic the schematic file to load
+         */
+        public SchematicLoader( File schematic ) {
+            this.schematicFile = schematic;
+        }
+
+        /**
+         * Loads a schematic file.
+         *
+         * @return the schematic file;
+         */
+        public FunoSchematic load() throws Exception {
+            NBTStream nbt = new NBTStream( new GZIPInputStream( new FileInputStream( this.schematicFile ) ), ByteOrder.BIG_ENDIAN );
+            FunoSchematic.SchematicData data = new FunoSchematic.SchematicData();
+
+            nbt.addListener( ( path, value ) -> {
+                switch( path ) {
+                    case "Schematic.Width":
+                        data.width = (short) value;
+                        break;
+                    case "Schematic.Length":
+                        data.length = (short) value;
+                        break;
+                    case "Schematic.Height":
+                        data.height = (short) value;
+                        break;
+                    case "Schematic.Materials":
+                        //data.materials = (String) value;
+                        break;
+                    case "Schematic.Blocks":
+                        data.blocks = (byte[]) value;
+                        break;
+                    case "Schematic.Data":
+                        data.blockData = (byte[]) value;
+                        break;
+                }
+            } );
+
+            nbt.parse();
+            return new FunoSchematic( data );
+        }
+    }
+
+    public static class SchematicData {
+        public short width;
+        public short length;
+        public short height;
+        //public String materials;
+        public byte[] blocks;
+        public byte[] blockData;
+
+        public SchematicData() {}
+
+        public SchematicData( short width, short length, short height ) {
+            this.width = width;
+            this.length = length;
+            this.height = height;
+        }
+
+        public SchematicData( short width, short length, short height, byte[] blocks ) {
+            this.width = width;
+            this.length = length;
+            this.height = height;
+            this.blocks = blocks;
+        }
+
+        public SchematicData( short width, short length, short height, byte[] blocks, byte[] blockData ) {
+            this.width = width;
+            this.length = length;
+            this.height = height;
+            this.blocks = blocks;
+            this.blockData = blockData;
+        }
+    }
+}
